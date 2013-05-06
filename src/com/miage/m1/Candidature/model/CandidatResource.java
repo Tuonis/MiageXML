57519b4c0453de43e8eb1a7f2c6eb0a1a131f4de
Kentish <Kentish@Kentish-HP>
2013-05-06 18:46:36 +0200

    modif 1

--- src/com/miage/m1/Candidature/model/CandidatResource.java
+++ src/com/miage/m1/Candidature/model/CandidatResource.java
@@ -9,6 +9,16 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import org.restlet.data.CharacterSet;
+import org.restlet.data.Form;
+import org.restlet.data.MediaType;
+import org.restlet.data.Status;
+import org.restlet.ext.xml.DomRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 
 /**
@@ -16,5 +26,37 @@ import java.util.List;
  * @author Tuonis Home
  */
 public class CandidatResource extends ServerResource {
+    
+    /**
+     * Id du produit correspondant
+     */
+    int id;
+    /**
+     * Le candidat correspondant
+     */
+    Candidat candidat;
+    /**
+     * Representation retournée
+     */
+    Representation resultat;
+    /**
+     * Erreurs possibles
+     */
+    List<String> erreurs;
+    
+    protected void init() {
+        String idAttribute = getRequest().getAttributes().get("id").toString();
+        try {
+            id = Integer.parseInt(idAttribute);
+            if (id <= 0) {
+                throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "idNotPositiveInteger");
+            }
+        } catch (NumberFormatException exc) {
+            // Indiquer que la requete est mal formee
+            throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "idNotInteger");
+        }
+    }
+    
+    
     
 }
